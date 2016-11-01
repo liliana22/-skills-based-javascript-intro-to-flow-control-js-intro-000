93658c70ea6a3a37c2d24d7d54a1c318e173df5f
lilianav <liliana22valenzuela@gmail.com>
2016-11-01 18:13:16 +0000

    i am ready

---
+++ index.js
@@ -0,0 +1,39 @@
+    // write your code below!
+
+function basicTeenager(age) {
+  if (age >= 13 && age <= 19){
+      return "You are a teenager!";
+  } else {
+    return undefined;
+  }
+}
+function teenager(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager!";
+  } else {
+  return "You are not a teenager";
+  }
+}
+
+    function ageChecker(age) {
+      if (age >= 13 && age <= 19){
+        return "You are a teenager!";
+      } if ( age <=12 ){
+         return "You are a kid"}
+         if (age <=29 ){
+           return "You are a grownup";
+         }
+}
+
+function ternaryTeenager(age) {
+  if (age >= 13 && age <= 19){
+    return "You are a teenager"
+  } if (age === 75){
+    return "You are not a teenager"
+  }
+
+}
+
+function switchAge(age) {
+
+}
